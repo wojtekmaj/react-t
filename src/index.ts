d8b3f6a4bd973ce0cde830684dad1b802b9f5976
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-06-05 22:40:35 +0200

    Simplify component props type export

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import useTranslation from './useTranslation';
 import TProvider from './TProvider';
 import T from './T';
 
-import type { TProviderProps } from './TProvider';
-import type { TProps } from './T';
+export type { TProviderProps } from './TProvider';
+export type { TProps } from './T';
 
 export { useTranslation, TProvider, T };
-
-export type { TProviderProps, TProps };
 
 export default T;
