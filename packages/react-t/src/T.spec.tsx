4295c8bfef4e4ec7689f44953f9aec8000db1947
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-04-26 13:04:15 +0200

    Use new JSX transform

--- packages/react-t/src/T.spec.tsx
+++ packages/react-t/src/T.spec.tsx
@@ -1,5 +1,4 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import React from 'react';
 import { act, render } from '@testing-library/react';
 import { getUserLocales } from 'get-user-locale';
 
