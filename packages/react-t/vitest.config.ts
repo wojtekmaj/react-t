876f53183176331c80a24e415d8696c47a7b7c9e
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-08-05 14:13:02 +0200

    Replace Happy DOM with Vitest Browser Mode

--- packages/react-t/vitest.config.ts
+++ packages/react-t/vitest.config.ts
@@ -1,10 +1,16 @@
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vitest/config';
 
 import type { ViteUserConfig } from 'vitest/config';
 
 const config: ViteUserConfig = defineConfig({
   test: {
-    environment: 'happy-dom',
+    browser: {
+      enabled: true,
+      headless: true,
+      instances: [{ browser: 'chromium' }],
+      provider: playwright(),
+    },
     setupFiles: 'vitest.setup.ts',
     watch: false,
   },
