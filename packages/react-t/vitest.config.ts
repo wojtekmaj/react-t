3efebe4c0fe45ba715cc569790b90533ca6bc80c
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-09-26 16:12:43 +0200

    Fix compatibility with isolatedDeclarations

--- packages/react-t/vitest.config.ts
+++ packages/react-t/vitest.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+import type { ViteUserConfig } from 'vitest/config';
+
+const config: ViteUserConfig = defineConfig({
   test: {
     environment: 'happy-dom',
     setupFiles: 'vitest.setup.ts',
     watch: false,
   },
 });
+
+export default config;
